adda215a9ed322b77e10b8c869d6667e6e8fe0e5
Rahulkumar Gayatri <rgayatri@lbl.gov>
2022-12-15 19:28:20 -0800

    Edits made to Makefile and axpy header file.

--- axpy/axpy.hpp
+++ axpy/axpy.hpp
@@ -46,104 +46,112 @@
 #include <cmath>
 
 struct AXPBY {
-  using view_t = Kokkos::View<double *>;
-  int N;
-  view_t x, y, z;
+    using view_t = Kokkos::View<double *>;
+    int N;
+    view_t x, y, z;
 
-  bool fence_all;
-  AXPBY(int N_, bool fence_all_)
-      : N(N_), x(view_t("X", N)), y(view_t("Y", N)), z(view_t("Z", N)),
-        fence_all(fence_all_) {}
+    bool fence_all;
+    AXPBY(int N_, bool fence_all_)
+        : N(N_),
+          x(view_t("X", N)),
+          y(view_t("Y", N)),
+          z(view_t("Z", N)),
+          fence_all(fence_all_) {}
 
-  KOKKOS_FUNCTION
-  void operator()(int i) const { z(i) = x(i) + y(i); }
+    KOKKOS_FUNCTION
+    void operator()(int i) const { z(i) = x(i) + y(i); }
 
-  double kk_axpby(int R) {
-    // Warmup
-    Kokkos::parallel_for("kk_axpby_wup", N, *this);
-    Kokkos::fence();
-
-    Kokkos::Timer timer;
-    for (int r = 0; r < R; r++) {
-      Kokkos::parallel_for("kk_axpby", N, *this);
-      if (fence_all)
+    double kk_axpby(int R) {
+        // Warmup
+        Kokkos::parallel_for("kk_axpby_wup", N, *this);
         Kokkos::fence();
+
+        Kokkos::Timer timer;
+        for (int r = 0; r < R; r++) {
+            Kokkos::parallel_for("kk_axpby", N, *this);
+            if (fence_all) Kokkos::fence();
+        }
+        Kokkos::fence();
+        double time = timer.seconds();
+        return time;
     }
-    Kokkos::fence();
-    double time = timer.seconds();
-    return time;
-  }
 
 #ifdef KOKKOS_ENABLE_OPENMPTARGET
-  double ompt_axpby(int R) {
-    AXPBY f(*this);
-    int N_ = N;
+    double ompt_axpby(int R) {
+        AXPBY f(*this);
+        int N_ = N;
 
 // Warmup
 #pragma omp target teams distribute parallel for simd data map(to : f, N_)
-    for (int i = 0; i < N_; i++) {
-      f(i);
-    }
-    Kokkos::fence();
+        for (int i = 0; i < N_; i++) {
+            f(i);
+        }
+        Kokkos::fence();
 
-    Kokkos::Timer timer;
+        Kokkos::Timer timer;
 
-    for (int r = 0; r < R; r++) {
+        for (int r = 0; r < R; r++) {
 #pragma omp target teams distribute parallel for simd data map(to : f, N_)
-      for (int i = 0; i < N_; i++) {
-        f(i);
-      }
-      if (fence_all)
+            for (int i = 0; i < N_; i++) {
+                f(i);
+            }
+            if (fence_all) Kokkos::fence();
+        }
+
         Kokkos::fence();
+        double time = timer.seconds();
+        return time;
     }
 
-    Kokkos::fence();
-    double time = timer.seconds();
-    return time;
-  }
+    double ompt_raw_axpby(int R) {
+        double *xp = x.data();
+        double *yp = y.data();
+        double *zp = z.data();
+        int N_ = N;
 
-  double ompt_raw_axpby(int R) {
-    double *xp = x.data();
-    double *yp = y.data();
-    double *zp = z.data();
-    int N_ = N;
 // Warmup
-#pragma omp target teams distribute parallel for simd is_device_ptr(           \
-    xp, yp, zp) data map(to                                                    \
-                         : N_)
-    for (int i = 0; i < N_; i++) {
-      zp[i] = xp[i] + yp[i];
-    }
-    Kokkos::fence();
-
-    Kokkos::Timer timer;
-    for (int r = 0; r < R; r++) {
-#pragma omp target teams distribute parallel for simd is_device_ptr(           \
-    xp, yp, zp) data map(to                                                    \
-                         : N_)
-      for (int i = 0; i < N_; i++) {
-        zp[i] = xp[i] + yp[i];
-      }
-      if (fence_all)
+#pragma omp target teams distribute parallel for simd is_device_ptr( \
+        xp, yp, zp) data                                             \
+map(to : N_)
+        for (int i = 0; i < N_; i++) {
+            zp[i] = xp[i] + yp[i];
+        }
         Kokkos::fence();
+
+        Kokkos::Timer timer;
+        for (int r = 0; r < R; r++) {
+#pragma omp target teams distribute parallel for simd is_device_ptr( \
+        xp, yp, zp) data                                             \
+map(to : N_)
+            for (int i = 0; i < N_; i++) {
+                zp[i] = xp[i] + yp[i];
+            }
+            if (fence_all) Kokkos::fence();
+        }
+        Kokkos::fence();
+        double time = timer.seconds();
+        return time;
     }
-    Kokkos::fence();
-    double time = timer.seconds();
-    return time;
-  }
 #endif
 
-  void run_test(int R) {
-    double bytes_moved = 1. * sizeof(double) * N * 3 * R;
-    double GB = bytes_moved / 1024 / 1024 / 1024;
-    double time_kk = kk_axpby(R);
-    printf("\n\nBytes moved[GBs] = %f\n",GB);
-    printf("AXPBY KK: %e s %e GB/s\n", time_kk, GB / time_kk);
+    void run_test(int R) {
+        double bytes_moved = 1. * sizeof(double) * N * 3 * R;
+        double GB = bytes_moved / 1024 / 1024 / 1024;
+        printf("\n\nBytes moved[GBs] = %f\n", GB);
+
+        // Kokkos version
+        double time_kk = kk_axpby(R);
+        printf("AXPBY KK: %e s %e GB/s\n", time_kk, GB / time_kk);
 #ifdef KOKKOS_ENABLE_OPENMPTARGET
-    double time_ompt = ompt_axpby(R);
-    printf("AXPBY OMPT: %e s %e GB/s\n", time_ompt, GB / time_ompt);
-    double time_ompt_raw = ompt_raw_axpby(R);
-    printf("AXPBY OMPT_RAW: %e s %e GB/s\n", time_ompt_raw, GB / time_ompt_raw);
+
+        // Native OpenMPTarget version that passes a functor to the target region.
+        double time_ompt = ompt_axpby(R);
+        printf("AXPBY OMPT: %e s %e GB/s\n", time_ompt, GB / time_ompt);
+
+        // Native OpenMPTarget version that uses raw pointers instead of a functor.
+        double time_ompt_raw = ompt_raw_axpby(R);
+        printf("AXPBY OMPT_RAW: %e s %e GB/s\n", time_ompt_raw,
+               GB / time_ompt_raw);
 #endif
-  }
+    }
 };
