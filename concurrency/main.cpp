6f08b97886d435a758d5ed52592d9db3558731dd
Rahulkumar Gayatri <rgayatri@lbl.gov>
2023-09-08 12:54:26 -0700

    Replace OpenMP routine.

--- concurrency/main.cpp
+++ concurrency/main.cpp
@@ -1,17 +1,16 @@
 #include <Kokkos_Core.hpp>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     int n_omp = 0, n_kk = 0;
 #if defined(KOKKOS_ENABLE_OPENMPTARGET)
-#pragma omp target map(tofrom: n_omp)
-    n_omp = getNumberofProcessorElements();
+#pragma omp target map(tofrom : n_omp)
+    n_omp = omp_get_num_procs();
 #endif
-    printf("OMP: n = %d\n",n_omp);
+    printf("OMP: n = %d\n", n_omp);
     Kokkos::initialize();
     {
         n_kk = Kokkos::DefaultExecutionSpace().concurrency();
-        printf("KK: n = %d\n",n_kk);
+        printf("KK: n = %d\n", n_kk);
     }
     Kokkos::finalize();
 
